d646d9652ed0339e9a64f5d8eb45dc59664b730a
John Gile <jgile@cliquestudios.com>
2017-05-20 11:59:39 -0500

    add dd and d

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,21 @@
 <?php
 
+    if ( ! function_exists('dd')) {
+        function dd()
+        {
+            $args = func_get_args();
+            call_user_func_array('dump', $args);
+            die();
+        }
+    }
+    if ( ! function_exists('d')) {
+        function d()
+        {
+            $args = func_get_args();
+            call_user_func_array('dump', $args);
+        }
+    }
+
     if ( ! function_exists('array_is_associative')) {
         /**
          * Determine if an array is associative or sequential.
